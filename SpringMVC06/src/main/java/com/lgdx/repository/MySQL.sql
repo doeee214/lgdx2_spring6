4a39fe323ff8f607523a46d537e7b9bf725f27b8
KDE <kim0214merry@gmail.com>
2025-05-16 10:14:13 +0900

    0516 프로젝트 commit

---
+++ SpringMVC06/src/main/java/com/lgdx/repository/MySQL.sql
@@ -0,0 +1,18 @@
+
+
+INSERT INTO BOARD(TITLE,CONTENTS,WRITER)
+VALUES('빠따들아 오늘 승리 챙겨와라','0승이 말이되냐','김도현');
+
+INSERT INTO BOARD(TITLE,CONTENTS,WRITER)
+VALUES('안녕하세요.이범호입니다','오늘부로 감독 사퇴합니다','이범호');
+
+INSERT INTO BOARD(TITLE,CONTENTS,WRITER)
+VALUES('나한텐 이번주가 개막이다','김도영 왔으니까 ','김도은');
+
+
+COMMIT;
+
+SELECT * FROM BOARD;
+
+
+
